fd1be1ad462a24e4ef79429043937e27a6197467
tramin238 <minhofroyal238@gmail.com>
2023-12-07 00:18:22 +0700

    Kết nối database

---
+++ Core/db.php
@@ -0,0 +1,18 @@
+<?php
+class Database {
+    private $host = 'localhost';
+    private $db_name = 'bignew';
+    private $username = 'root';
+    private $password = '';
+    private $conn;
+
+    public function getConnection() {
+        $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+
+        return $this->conn;
+    }
+}
